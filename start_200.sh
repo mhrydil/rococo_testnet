917abe7ae60d2eb86801cac331c0e48125b623a9
Matthew Hrydil <mhrydil@gmail.com>
2020-11-11 18:10:44 -0500

    add start_200 & 300 back

--- start_200.sh
+++ start_200.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+
 # start parachain 200
 ./substrate-parachain-template/target/release/parachain-collator --tmp --ws-port 9977 --port 30336 --parachain-id 200 --validator -- --chain rococo-3.json
